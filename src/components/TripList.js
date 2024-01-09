900763c37b67bb833f7d5a7a2d6f32a4d9c8286f
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2024-01-09 12:53:24 +0100

    Make the url to fetch trips dynamic

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -5,17 +5,18 @@ import "./TripList.css";
 
 const TripList = () => {
   const [trips, setTrips] = useState([]);
+  const [url, setUrl] = useState("http://localhost:3005/trips");
 
   useEffect(() => {
-    axios.get("http://localhost:3005/trips").then((result) => {
+    axios.get(url).then((result) => {
       setTrips(result.data);
     });
 
     // Cleaning callback
     return () => {
-      // Do nothing for now;
+      // Do nothing for now
     };
-  }, []);
+  }, [url]);
 
   return (
     <div className="trip-list">
@@ -28,6 +29,16 @@ const TripList = () => {
           </li>
         ))}
       </ul>
+      <div className="filters">
+        <button
+          onClick={() => setUrl("http://localhost:3005/trips?location=Europe")}
+        >
+          European Trips
+        </button>
+        <button onClick={() => setUrl("http://localhost:3005/trips")}>
+          All Trips
+        </button>
+      </div>
     </div>
   );
 };
